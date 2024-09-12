073f3ac765c286dd29654b2e2842e5c61e54c78b
Andebugulin <gulin.andrey2005@gmail.com>
2024-09-12 12:00:58 +0300

    update(adapt-to-run-on-linux)

--- settings_level.py
+++ settings_level.py
@@ -1,12 +1,12 @@
 import os, random
 
 def level_loading():   #загрузка уровней из тестого файлов рандомным образом
-    with open(str(os.getcwd()) + '\data\levels' + '\\' +
-              random.choice(os.listdir(str(os.getcwd()) + '\data\levels')), 'r', encoding='UTF-8') as file_level:
+    with open(str(os.getcwd()) + '/data/levels' + '//' +
+              random.choice(os.listdir(str(os.getcwd()) + '/data/levels')), 'r', encoding='UTF-8') as file_level:
         data_level = list(map(lambda x: x.replace('\n', ''), file_level.readlines()))
 
-    with open(str(os.getcwd()) + '\data\levels' + '\\' +
-              random.choice(os.listdir(str(os.getcwd()) + '\data\levels')), 'r', encoding='UTF-8') as file_level_second:
+    with open(str(os.getcwd()) + '/data/levels' + '//' +
+              random.choice(os.listdir(str(os.getcwd()) + '/data/levels')), 'r', encoding='UTF-8') as file_level_second:
         data_level_second = list(map(lambda x: x.replace('\n', ''), file_level_second.readlines()))
     data_level_final = []
     for index, string in enumerate(data_level):
